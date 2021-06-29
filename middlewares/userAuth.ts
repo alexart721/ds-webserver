dfd071d38377cb14faba6ac4047a5beee3fdecf2
Ryan Baker <rbaker699@gmail.com>
2021-06-29 08:54:14 -0700

    Refactor the way middlewares work to cascade off each other

--- middlewares/userAuth.ts
+++ middlewares/userAuth.ts
@@ -8,13 +8,13 @@ const { SECRET_KEY } = process.env;
 
 const userAuth = async (req: Request, res: Response, next: NextFunction) => {
   const authHeaders = req.headers.authorization;
-  if (!authHeaders) return res.sendStatus(403);
+  if (!authHeaders) return res.sendStatus(400);
   const token = authHeaders.split(' ')[1];
   const redisClient = req.app.locals.client;
 
   redisClient.get(`blacklist_${token}`, (err: RedisError, data: string) => {
-    if (err) return res.status(400).send({ err, message: 'An error occurred, logging out.' });
-    if (data) return res.status(400).send({ message: 'Please login again.' });
+    if (err) return res.status(500).send({ err, message: 'An error occurred.' });
+    if (data) return res.status(401).send({ message: 'Please login again.' });
   });
 
   try {
@@ -25,9 +25,10 @@ const userAuth = async (req: Request, res: Response, next: NextFunction) => {
       res.locals.user = user;
       res.locals.tokenExp = exp;
       res.locals.token = token; // Might not need this
+      res.locals.roles = user.roles;
       next();
     } else {
-      res.sendStatus(401);
+      res.sendStatus(500);
     }
   } catch (error) {
     res.sendStatus(401);
