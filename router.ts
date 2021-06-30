0973c1a83df694d4191f2da01b3b91476170009c
Ryan Baker <rbaker699@gmail.com>
2021-06-30 12:15:14 -0700

    Update to point to auth server (Incomplete) for code review

--- router.ts
+++ router.ts
@@ -5,41 +5,42 @@ import dmChannel from './controllers/dmChannel';
 import user from './controllers/user';
 import userAuth from './middlewares/userAuth';
 import adminAuth from './middlewares/adminAuth';
+import auth from './middlewares/auth';
 
 const router = express.Router();
 
 // Issue routes
-router.get('/issues/:id', userAuth, issue.getIssue);
-router.put('/issues/:id', userAuth, issue.updateIssue);
-router.put('/issues/:id/resolve', userAuth, issue.resolveIssue);
-router.put('/issues/:id/messages/send', userAuth, issue.addMessageToIssue);
-router.get('/issues/archived', userAuth, issue.getArchivedIssues);
+router.get('/issues/:id', auth('User'), issue.getIssue);
+router.put('/issues/:id', auth('User'), issue.updateIssue);
+router.put('/issues/:id/resolve', auth('User'), issue.resolveIssue);
+router.put('/issues/:id/messages/send', auth('User'), issue.addMessageToIssue);
+router.get('/issues/archived', auth('User'), issue.getArchivedIssues);
 
 // Channel routes
-router.get('/channels', userAuth, channel.getAllChannels);
-router.post('/channels', userAuth, adminAuth, channel.createNewChannel); // ADMIN
-router.delete('/channels/:id', userAuth, adminAuth, channel.deleteOneChannel); // ADMIN
-router.get('/channels/:id/issues', userAuth, channel.getChannelIssues);
-router.post('/channels/:id/issues', userAuth, channel.addNewIssue);
+router.get('/channels', auth('User'), channel.getAllChannels);
+router.post('/channels', auth('Admin'), channel.createNewChannel); // ADMIN
+router.delete('/channels/:id', auth('Admin'), channel.deleteOneChannel); // ADMIN
+router.get('/channels/:id/issues', auth('User'), channel.getChannelIssues);
+router.post('/channels/:id/issues', auth('User'), channel.addNewIssue);
 
 // DM Channel routes
-router.get('/dm', userAuth, dmChannel.getAllDmChannelForUser);
-router.post('/dm', userAuth, dmChannel.createNewDmChannel);
-router.get('/dm/:id', userAuth, dmChannel.getDmChannelById);
-router.post('/dm/:id', userAuth, dmChannel.addNewMessage);
-router.delete('/dm/:id', userAuth, dmChannel.deleteDmChannel);
+router.get('/dm', auth('User'), dmChannel.getAllDmChannelForUser);
+router.post('/dm', auth('User'), dmChannel.createNewDmChannel);
+router.get('/dm/:id', auth('User'), dmChannel.getDmChannelById);
+router.post('/dm/:id', auth('User'), dmChannel.addNewMessage);
+router.delete('/dm/:id', auth('User'), dmChannel.deleteDmChannel);
 
 // User routes
-router.get('/users/:id', userAuth, user.getUserById);
+router.get('/users/:id', auth('User'), user.getUserById);
 router.post('/users', user.createNewUser);
 router.post('/users/login', user.loginUser);
 router.post('/users/register', user.registerUser);
-router.post('/users/logout', userAuth, user.logoutUser);
-router.put('/users/channel/:id', userAuth, user.addChannel);
-router.delete('/users/channels/:id', userAuth, user.deleteChannelFromUserList);
-router.put('/users/:id/approve', userAuth, adminAuth, user.approveUser); // ADMIN
-router.put('/users/:id/deny', userAuth, adminAuth, user.bannedUser); // ADMIN
-router.put('/users/:id', userAuth, adminAuth, user.updateUserInfo); // ADMIN
-router.delete('/users/:id', userAuth, adminAuth, user.deleteUser); // ADMIN
+router.post('/users/logout', auth('User'), user.logoutUser);
+router.put('/users/channel/:id', auth('User'), user.addChannel);
+router.delete('/users/channels/:id', auth('User'), user.deleteChannelFromUserList);
+router.put('/users/:id/approve', auth('Admin'), user.approveUser); // ADMIN
+router.put('/users/:id/deny', auth('Admin'), user.bannedUser); // ADMIN
+router.put('/users/:id', auth('Admin'), user.updateUserInfo); // ADMIN
+router.delete('/users/:id', auth('Admin'), user.deleteUser); // ADMIN
 
 export default router;
