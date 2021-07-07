c98cd5fbb6cee2e8bd87d289edc98d4d1b5dce8f
Alex Teodorescu <ateodorescu721@gmail.com>
2021-07-07 09:55:48 -0400

    socket updates

--- sockets/index.ts
+++ sockets/index.ts
@@ -1,25 +1,32 @@
 import socketIO from 'socket.io';
+
 const io = new socketIO.Server();
 
 io.on('connection', (socket) => {
   console.log('is connected');
   socket.emit('test', 'This worked!');
+  socket.on('join_room', (room) => {
+    socket.join(room);
+  });
+  socket.on('message', (message) => {
+    console.log(message);
+    socket.emit('broadcast_message', message.message);
+  });
 });
 
 io.on('connect_error', (err) => {
   console.log(`Connection error: ${err}`);
 });
 
-function init (server: any) {
+function init(server: any) {
   io.attach(server, {
     serveClient: false,
     cors: {
       origin: '*',
-    }
+    },
   });
 }
 
-
 export default {
-  init
+  init,
 };
